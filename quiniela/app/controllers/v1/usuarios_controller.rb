8422c0321d02adfce277b8be649da7e47e7acf0d
Alexandra <alemoli9715@gmail.com>
2018-05-31 21:33:17 -0600

    login

--- quiniela/app/controllers/v1/usuarios_controller.rb
+++ quiniela/app/controllers/v1/usuarios_controller.rb
@@ -9,12 +9,30 @@ class V1::UsuariosController < ApplicationController
     if usuario
       render json:{status: "Error", message: "El usuario ya existe"}
     else
-      user = Usuario.new(nombre: params[:nombre], email: params[:email], contrasena: [:contrasena], authentication_token: auth_token)
+      user = Usuario.new(nombre: params[:nombre], email: params[:email], contrasena: params[:contrasena], authentication_token: auth_token)
       if user.save
-        render json: {status: "Sucess", message: "Usuario creado", usuario:user}, status: :created
+        render json: {status: "Success", message: "Usuario creado", usuario:user}, status: :created
       else
         render json:{status: "Error", message: user.errors}, status: :bad
       end
+    end
+  end
+  def login
+    usuario = Usuario.where(email: params[:email]).first
+    contrasena = params[:contrasena]
+
+    if usuario
+      if (usuario.contrasena == contrasena)
+        token = [('a'..'z'),('A'..'Z'),('0'..'9')].map(&:to_a).flatten
+        auth_token = (0...20).map{token[rand(token.length)]}.join
+        usuario.authentication_token = auth_token
+        usuario.save
+        render json:{status: "Success", message: "Log in", data: usuario}, status: :created
+      else
+        render json:{status: "Error", message: "Incorrect Password"}, status: :bad
+      end
+    else
+      render json:{status: "Error", message: "Usuario no existe"}, status: :bad
     end
   end
 end
--- quiniela/config/routes.rb
+++ quiniela/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: :json } do
     resources :usuarios, only: [:create]
+    post'usuarios/login', to:'usuarios#login'
   end
 
 end
